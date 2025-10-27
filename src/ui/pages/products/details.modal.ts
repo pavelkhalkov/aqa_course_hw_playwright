e5b215f440d1c1b11f5cae9814949ac4c2a6884a
pkhalkov <pavel.khalkov@mail.ru>
2025-10-27 11:37:57 +0300

    Completed HW-23

--- src/ui/pages/products/details.modal.ts
+++ src/ui/pages/products/details.modal.ts
@@ -3,13 +3,13 @@ import { SalesPortalPage } from "../salesPortal.page";
 import { MANUFACTURERS } from "data/salesPortal/products/manufacturers";
 
 export class ProductDetailsModal extends SalesPortalPage {
-  readonly uniqueElement = this.page.locator("#ProductDetailsModal");
-
+  readonly uniqueElement = this.page.locator("div[role='dialog']");
   readonly title = this.uniqueElement.locator("h5");
   readonly closeButton = this.uniqueElement.locator("button.btn-close");
   readonly editButton = this.uniqueElement.locator("button.btn-primary");
   readonly cancelButton = this.uniqueElement.locator("button.btn-secondary");
-
+  readonly deleteProductButton = this.uniqueElement.locator('button', { hasText: 'Yes, Delete' });
+ 
   readonly productValue = this.uniqueElement.locator("p");
 
   async clickClose() {
